204ff95c679fc64271c5a8b619f6510529d345ba
matt hardy <matthewhardy17@gmail.com>
2020-10-19 21:35:17 +0100

    ah haaaa alan partridge gif included

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif-list';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
+    const gifs = [
+      { id: 'ZsqYWarAFJw0o' }
+    ];
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
+          <div className="selected-gif">
+            <Gif id="ZsqYWarAFJw0o" />
+          </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class Gif extends Component {
   render() {
+    const src = `https://media0.giphy.com/media/${this.props.id}/giphy.gif`;
     return (
-      
-    )
+      <img src={src} alt="" className="gif" />
+    );
   }
 }
 
-export default Gif
+export default Gif;
