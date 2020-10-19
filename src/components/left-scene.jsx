571f27908108e0762c3577e750354515dbc121cc
matt hardy <matthewhardy17@gmail.com>
2020-10-19 20:30:51 +0100

    started gif porject

---
+++ src/components/left-scene.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class LeftScene extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { changed: false };
+  }
+
+  handleChange = () => {
+    this.setState({ changed: !this.state.changed });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene" onChange={this.handleChange} />
+      </div>
+    )
+  }
+}
+
+export default LeftScene;
